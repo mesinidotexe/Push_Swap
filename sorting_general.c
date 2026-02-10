b75e54e0c893f31991418ea1a3c024424719bc60
Victor Mesini Costa <vmesini-@c2r4s1.42lisboa.com>
2026-02-10 18:03:24 +0000

    auto

--- sorting_general.c
+++ sorting_general.c
@@ -12,25 +12,44 @@
 
 #include "push_swap.h"
 
+int	bits_needed(int n)
+{
+	int	bits;
+
+	bits = 0;
+	while (n > 0)
+	{
+		n >>= 1;
+		bits++;
+	}
+	return (bits);
+}
+
 void	general(t_stack **a, t_stack **b)
 {
 	int	i;
+	int	bits;
 	int	size;
 
 	if (!a || !*a)
 		return ;
-	i = 0;
 	size = ps_lstsize(*a);
+	i = 0;
 	indexing(*a);
-	while (size > 0)
+	bits = bits_needed((*a)->index);
+	while (bits > 0)
 	{
-		if (((*a)->index >> i) & 1)
-			ra(*a);
-		else
-			pb (b, a);
-		size--;
+		while (size > 0)
+		{
+			if (((*a)->index >> i) & 1)
+				ra(*a);
+			else
+				pb(b, a);
+			size--;
+		}
 		i++;
+		while (pa(a, b))
+			;
+		bits--;
 	}
-	while (pa(a, b))
-		;
 }
